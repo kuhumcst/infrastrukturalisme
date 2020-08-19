bdce7b2bcd6bc38c66682f3f48ce8539a531500a
Sofie Vonge Jensen <sofievj_89@hotmail.com>
2020-08-19 10:48:49 +0200

    semester data, regex for json

---
+++ data/timeline/reqex.py
@@ -0,0 +1,27 @@
+import re
+
+with open("result.txt", "a", encoding="utf8") as wf:
+    pattern = re.compile(r"(\d{4}),\s+(II?):\s+(.+)")
+
+    with open("aarhus.txt", "r", encoding="utf8") as file:
+        line = file.readline()
+
+        while line:
+            result = pattern.sub(r'{"place": "Aarhus", ”year”: \1, ”semester”: ”\2”, ”description”: ”\3”}', line)
+
+            line = file.readline()
+            output = result.strip()
+            if output != "":
+                wf.write(output + "\n")
+
+    with open("cph.txt", "r", encoding="utf8") as file:
+        line = file.readline()
+
+        while line:
+            result = pattern.sub(r'{"place": "CPH", ”year”: \1, ”semester”: ”\2”, ”description”: ”\3”}', line)
+
+            line = file.readline()
+            output = result.strip()
+            if output != "":
+
+                wf.write(output + "\n")
