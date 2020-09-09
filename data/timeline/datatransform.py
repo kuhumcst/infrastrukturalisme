70e357c217a7b6aae1334ca938e9a6ff6181a993
Sofie Vonge Jensen <sofievj_89@hotmail.com>
2020-09-09 10:30:27 +0200

    each subject in Undervisning is now a string in an array

--- data/timeline/datatransform.py
+++ data/timeline/datatransform.py
@@ -151,10 +151,10 @@ with open("result.txt", "r", encoding="utf-8") as result_file:
         dict["Institution"] = item[0]["Institution"]
         dict["Kategori"] = "Undervisning"
 
-        description = str(subjects) + " fag"
+        description = [str(subjects) + " fag"]
 
         for i in range(subjects):
-            description += "\n" + item[i]["Event"]
+            description.append(item[i]["Event"])
 
         dict["Event"] = description
 
