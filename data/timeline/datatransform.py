60b6b3f43073934b12dbf97c168ea717d75c9bd4
Sofie Vonge Jensen <sofievj_89@hotmail.com>
2020-09-09 12:03:32 +0200

    added comments to the datatransform file

--- data/timeline/datatransform.py
+++ data/timeline/datatransform.py
@@ -7,7 +7,7 @@ import operator
 
 data = []
 
-
+# Function for checking if the provided dates are formatted as real dates
 def is_date(str):
     try:
         day, month, year = str.split('-')
@@ -19,10 +19,11 @@ def is_date(str):
     except ValueError:
         return False
 
-
+# Working with the csv file
 with open("Hjelmslevkronologi.csv", mode="r", encoding="utf-8-sig") as file:
     reader = csv.DictReader(file, delimiter=";")
 
+# predefined categories and their abbreviation in the original dataset
     categories = {
         "L": "Liv",
         "U": "Undervisning",
@@ -33,6 +34,7 @@ with open("Hjelmslevkronologi.csv", mode="r", encoding="utf-8-sig") as file:
     }
 
     for row in reader:
+        # making a dict for each row in the original dataset to fill with modified and formatted data
         dict = {}
         
         if is_date(row["Startdato"]):
@@ -44,6 +46,7 @@ with open("Hjelmslevkronologi.csv", mode="r", encoding="utf-8-sig") as file:
         if is_date(row["Slutdato"]):
             dict["Slutdato"] = row["Slutdato"]
         elif not row["Slutdato"] and is_date(row["Startdato"]):
+            # making a data object to add a day correctly, formatting it and adding it as a Slutdato if there is no Slutdato in the original dataset
             date = datetime.datetime.strptime(row["Startdato"], "%d-%m-%Y").date()
             date = date + datetime.timedelta(days=1)
             date = date.strftime("%d-%m-%Y")
@@ -67,6 +70,7 @@ with open("Hjelmslevkronologi.csv", mode="r", encoding="utf-8-sig") as file:
         else:
             dict["Institution"] = None
 
+        # mapping the abbreviation to the correct values if the abbreviation exists in the original dataset
         if row["Kategori"]:
             if row["Kategori"] in categories.keys():
                 dict["Kategori"] = categories[row["Kategori"]]
@@ -75,6 +79,7 @@ with open("Hjelmslevkronologi.csv", mode="r", encoding="utf-8-sig") as file:
         else:
             dict["Kategori"] = None
 
+        # the event will be the same as the tooltip, since we would like to show the event as the tooltip
         if row["Event / Begivenhed"] and row["Event-beskrivelse"]:
             dict["Tooltip"] = row["Event / Begivenhed"] + ": " + row["Event-beskrivelse"]
             dict["Event"] = row["Event / Begivenhed"] + ": " + row["Event-beskrivelse"]
@@ -95,17 +100,24 @@ with open("Hjelmslevkronologi.csv", mode="r", encoding="utf-8-sig") as file:
         else:
             dict["Kilde"] = None
 
+        # only append the dict to the data if we have both a Startdato and a Slutdato
         if "Startdato" and "Slutdato" in dict.keys():
             data.append(dict)
 
+# working with the result file from regex.py
 with open("result.txt", "r", encoding="utf-8") as result_file:
+    # appending the dicts from the result file to a list, one by one
     init_list = []
     for line in result_file:
         dict = ast.literal_eval(line)
         init_list.append(dict)
 
+    # a list for the current data that has not been grouped by the Startdato
     cur_data = []
+
+    # count used for sorting the dict later in groupby
     count = 1
+
     for row in init_list:
         dict = {}
 
@@ -133,13 +145,16 @@ with open("result.txt", "r", encoding="utf-8") as result_file:
 
         cur_data.append(dict)
 
+    # sorting the current data
     cur_data.sort(key=operator.itemgetter("Sorting"))
 
     grouped_data = []
 
+    # grouping the data by Startdato and appending to a new list
     for key, items in itertools.groupby(cur_data, operator.itemgetter("Startdato")):
         grouped_data.append(list(items))
 
+    # making the actual dict to be appended to the new dataset
     for item in grouped_data:
         dict = {}
 
@@ -153,13 +168,15 @@ with open("result.txt", "r", encoding="utf-8") as result_file:
         dict["Institution"] = item[0]["Institution"]
         dict["Kategori"] = "Undervisning"
 
-        description = str(subjects) + " fag"
+        # for multiline text in the tooltip, each line has to be set up as an object
         desc_dict = {}
 
         for i in range(subjects):
             desc_dict[i + 1] = item[i]["Event"]
 
         dict["Tooltip"] = desc_dict
+
+        description = str(subjects) + " fag"
         dict["Event"] = description
 
         dict["Personer"] = None
@@ -167,6 +184,6 @@ with open("result.txt", "r", encoding="utf-8") as result_file:
 
         data.append(dict)
 
-
+# writing the data list with the dicts to a json file
 with open("hjelmslev.json", "a", encoding="utf-8") as json_file:
     json.dump(data, json_file, indent=2, ensure_ascii=False)
